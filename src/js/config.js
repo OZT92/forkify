a3f53fd0d16ae74a0efe827bb80cb5691ec1d035
OZT92 <ozturkyusa@gmail.com>
2022-07-03 16:26:50 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '37a2020f-7657-44d6-a45e-47dea04e3fbd';
+export const MODAL_CLOSE_SEC = 2.5;
